94f31ff38265f649a6b01b9be42ede0c6414a7d8
Nouah <noahgreatakoni@gmail.com>
2022-12-18 00:07:53 +0100

    student

---
+++ src/main/java/kpakurumo/school/africa/data/dto/request/StudentRegistrationRequest.java
@@ -0,0 +1,28 @@
+package kpakurumo.school.africa.data.dto.request;
+
+import kpakurumo.school.africa.data.models.Course;
+import lombok.*;
+import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Builder
+@Setter
+@Getter
+@Validated
+public class StudentRegistrationRequest{
+ private String firstName;
+ private String lastName;
+ @Email(message = "invalid email")
+ private String email;
+ private String phoneNumber;
+ private String password;
+ private String address;
+ //@OneToMany(fetch = FetchType.EAGER)
+// private Collection<Course> courses ;
+
+
+}
