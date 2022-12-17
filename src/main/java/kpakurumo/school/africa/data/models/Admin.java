00a1e934a61f4f3cb07842e374bfff8591efc935
Nouah <noahgreatakoni@gmail.com>
2022-12-17 23:01:56 +0100

    admin

---
+++ src/main/java/kpakurumo/school/africa/data/models/Admin.java
@@ -0,0 +1,24 @@
+package kpakurumo.school.africa.data.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+//@ToString
+public class Admin extends EarlyBrightAppUser {
+    @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE )
+    private Long id;
+    private String firstName;
+    private  String lastName;
+    private  String email;
+    private String phoneNumber;
+    private String password;
+    //private Staffs schoolStaff;
+
+}
