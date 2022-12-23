20e1bca7205cc293615636f1156b556966629cea
Nouah <noahgreatakoni@gmail.com>
2022-12-23 05:25:29 +0100

    admin

---
+++ src/main/java/kpakurumo/school/africa/repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package kpakurumo.school.africa.repository;
+
+import kpakurumo.school.africa.data.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findAdminByEmail(String email);
+}
