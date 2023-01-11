5f2181e38557918f7b2f15a353dbf6f38d4fc1f2
Nouah <noahgreatakoni@gmail.com>
2023-01-12 00:19:21 +0100

    student

---
+++ src/main/java/kpakurumo/school/africa/data/dto/respond/StudentRegistrationResponse.java
@@ -0,0 +1,11 @@
+package kpakurumo.school.africa.data.dto.respond;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+public class StudentRegistrationResponse {
+   // private Long id;
+    private String message;
+}
