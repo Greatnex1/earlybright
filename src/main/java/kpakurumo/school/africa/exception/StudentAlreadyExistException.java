6886522d76628b774444efa9e51b4548ef60ef90
Nouah <noahgreatakoni@gmail.com>
2023-01-15 20:58:56 +0100

    student

---
+++ src/main/java/kpakurumo/school/africa/exception/StudentAlreadyExistException.java
@@ -0,0 +1,7 @@
+package kpakurumo.school.africa.exception;
+
+public class StudentAlreadyExistException extends RuntimeException {
+    public StudentAlreadyExistException(String message) {
+        super(message);
+    }
+}
