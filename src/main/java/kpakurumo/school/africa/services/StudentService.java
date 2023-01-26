3d4896a91424b230a49174ad4babf635eefcf728
Nouah <noahgreatakoni@gmail.com>
2023-01-26 18:33:39 +0100

    student

--- src/main/java/kpakurumo/school/africa/services/StudentService.java
+++ src/main/java/kpakurumo/school/africa/services/StudentService.java
@@ -2,19 +2,19 @@ package kpakurumo.school.africa.services;
 
 import kpakurumo.school.africa.data.dto.request.StudentRegistrationRequest;
 import kpakurumo.school.africa.data.dto.respond.StudentRegistrationResponse;
+import kpakurumo.school.africa.data.models.Course;
 import kpakurumo.school.africa.data.models.Student;
 import kpakurumo.school.africa.exception.StudentAlreadyExistException;
 
 import java.util.List;
 import java.util.Optional;
 
-//@Service
 public interface StudentService {
     StudentRegistrationResponse register (StudentRegistrationRequest registerRequest) throws StudentAlreadyExistException;
 
+//    Course registerForCourse(Course course);
   List <Student> getAllStudents();
 
-  void expelStudent (Long studentId);
 
  Optional <Student> updateStudent  (Long studentId, Student student);
 
