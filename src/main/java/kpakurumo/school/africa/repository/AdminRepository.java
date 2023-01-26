ca359beef5b40a5a7d9ecf7635714ac8f336e279
Nouah <noahgreatakoni@gmail.com>
2023-01-26 15:54:17 +0100

    admin

--- src/main/java/kpakurumo/school/africa/repository/AdminRepository.java
+++ src/main/java/kpakurumo/school/africa/repository/AdminRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Optional<Admin> findAdminByEmail(String email);
+    Optional<Admin> findAdminByUsername(String email);
 }
